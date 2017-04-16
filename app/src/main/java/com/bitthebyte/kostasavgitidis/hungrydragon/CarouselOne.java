a9684cdf7d8de74c0b9d4dfa677011a0e19a3a24
Kostas Avgitidis <Kwnnax@gmail.com>
2017-04-17 02:01:21 +0300

    Second Commit

--- app/src/main/java/com/bitthebyte/kostasavgitidis/hungrydragon/CarouselOne.java
+++ app/src/main/java/com/bitthebyte/kostasavgitidis/hungrydragon/CarouselOne.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CarouselOne extends AppCompatActivity {
 
@@ -13,17 +14,18 @@ public class CarouselOne extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carousel_one);
         setTitle(R.string.emptyString);
+        onClick(buttonName);
     }
 
         public void carouselTwo(View view) {
             Intent intent = new Intent(this, CarouselTwo.class);
-            startActivity(intent); //start of CarouselTwo.java 
+            startActivity(intent); //start of CarouselTwo.java
         }
 
-    Button buttonName = (Button)findViewById(R.id.next_button);
+    ImageButton buttonName = (ImageButton)findViewById(R.id.next_button);
     public void onClick(View v) {
         if(v==buttonName){
-            buttonName.setBackgroundResource(R.drawable.right_arrow);
+            buttonName.setBackgroundResource(R.drawable.right_arrow3);
         }
     }
     }
